a6b168f19c7d4f0f90c0e64fc4bf40e30ab98152
Abdullahalbakhit <70815008+Abdullahalbakhit@users.noreply.github.com>
2020-09-12 16:58:10 +0300

    done

--- iOS-Hw1-Part1.playground/Contents.swift
+++ iOS-Hw1-Part1.playground/Contents.swift
@@ -5,12 +5,12 @@
  
  */
 
-let name = "Ahmad"
+var name = "Ahmad"
 
-name = "Salem"
+name = "Ahmad"
 
 
-var myWeight = 60
+var myWeight = 6.0
 var FriendWeight = 80.5
 var height = 1.5
 
@@ -38,37 +38,37 @@ var bmi = myWeight * (height * height)
  */
 
 //Person 1
-var firstName1 = ""
-var lastName1 = ""
-var email1 = ""
-var phoneNumber1 = ""
-var age1 = 0
-var country1 = ""
-var area1 = ""
-var password = ""
+var firstName1 = "عبدالله"
+var lastName1 = "البخيت"
+var email1 = "abdullah@gmail.com"
+var phoneNumber1 = "1234"
+var age1 = 11
+var country1 = "dubai"
+var area1 = "blok1"
+var password = "azx"
 var isKuwaiti = false
 
 //Person 2
-
-
-
-
-
-
-
-
-
+var firstName2 = "محمد"
+var lastName2 = "الوزان"
+var email2 = "mhd@gmail.com"
+var phoneNumber2 = "4567"
+var age2 = 12
+var country2 = "kuwait"
+var area2 = " blok2"
+var password2 = "abc"
+var isKuwaiti2 = true
 
 //Person 3
-
-
-
-
-
-
-
-
-
+var firstName3 = "عمر"
+var lastName3 = "الابراهيم"
+var email3 = "omar@gmail.com"
+var phoneNumber3 = "9988"
+var age3 = 24
+var country3 = "turki"
+var area3 = "tqseeem"
+var password3 = "gg"
+var isKuwaiti3 = false
 /*
 - - - -
 # المطلوب الثالث
@@ -82,3 +82,23 @@ var isKuwaiti = false
 
  
  */
+var passworDif = (password == password2) || (password == password3) || (password2 == password) || (password2 == password3) || (password3 == password) || (password3 == password2)
+print(passworDif)
+
+var ageDifference = (age1 == age2 ) || (age1 == age3) || (age2 == age1) || (age2 == age3) || (age3 == age1) || (age3 == age2)
+ print(ageDifference)
+
+var countryDiffrenence = (country1 == country2) || (country1 == country3) || (country2 == country1) || (country2 == country3) || (country3 == country1) || (country3 == country2)
+print(countryDiffrenence)
+
+var ageDif2 = (age1 > 18) || (age2 > 18) || (age3 > 18)
+print(ageDif2)
+
+var ageDif3 = (age1 < 18) || (age2 < 18) || (age3 < 18)
+print(ageDif3)
+
+var ageDif4 = (age1 == 17) || (age2 == 17) || (age3 == 17)
+print(ageDif4)
+
+var ageDif5 = (age1 != 17) || (age2 != 17) || (age3 != 17)
+print(ageDif5)
